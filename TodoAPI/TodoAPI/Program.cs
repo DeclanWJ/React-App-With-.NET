2bc84f6e54b96e6e077208a9bd087cad1677d111
DeclanWJ <declanj2000@hotmail.com>
2022-06-15 14:26:27 -0500

    Create Files for WebPage
    1. WWWROOT folder
    2. HTML/CSS/JS folders
    3. index.HTML = base webpage
    4. site.JS/site.CSS = site functions and styling

--- TodoAPI/TodoAPI/Program.cs
+++ TodoAPI/TodoAPI/Program.cs
@@ -8,14 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//  c.SwaggerDoc("v1", new() {Title = "TodoAPI, Version = "v1" });
-//});
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -23,8 +16,7 @@ var app = builder.Build();
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+ 
 }
 
 app.UseHttpsRedirection();
